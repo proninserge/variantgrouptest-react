9058374182b53edee4c7795068ec94bcc5ec69b2
Sergey Pronin <pronin_sergey@lenta.ru>
2026-05-23 22:57:58 +0600

    refactor: remove storage event

--- src/app/providers/ApplicationProvider.tsx
+++ src/app/providers/ApplicationProvider.tsx
@@ -1,8 +1,6 @@
 import type { ReactElement, ReactNode } from 'react';
 
-import {
-  useApplicationChannel,
-} from '@/entities/application';
+import { useApplicationChannel } from '@/entities/application';
 
 type ApplicationProviderProps = {
   children: ReactNode;
@@ -10,19 +8,6 @@ type ApplicationProviderProps = {
 
 export function ApplicationProvider({ children }: ApplicationProviderProps): ReactElement {
   useApplicationChannel();
-
-  // useEffect(() => {
-  //   function onStorageChange(event: StorageEvent) {
-  //     if (event.key === APPLICATIONS_STORAGE_KEY) {
-  //       void useApplicationStore.persist.rehydrate();
-  //     }
-  //   }
-
-  //   window.addEventListener('storage', onStorageChange);
-  //   return () => {
-  //     window.removeEventListener('storage', onStorageChange);
-  //   };
-  // }, []);
 
   return <>{children}</>;
 }
